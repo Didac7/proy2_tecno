983418e69499c7c6adf4910f7239ff5c2de755b3
Didac <ediego918@gmail.com>
2025-11-27 04:59:47 -0400

    vite base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/inf513/grupo09sc/proyecto2/proy2_tecno/public/build/',
+    base: '/inf513/grupo09sc/proyecto2/proy2_tecno/public/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
